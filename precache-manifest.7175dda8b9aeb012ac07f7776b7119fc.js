ca569c702d3827f481cbeffffbf65fb04a624a78
Vishal P <vishalprakaash@github.com>
2020-09-04 20:29:13 +0530

    Updates

---
+++ precache-manifest.7175dda8b9aeb012ac07f7776b7119fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7982d29ee3312e96b66a7b3b63db7d",
+    "url": "/vio-chat-app/index.html"
+  },
+  {
+    "revision": "3b9d4d11f8b3981e4432",
+    "url": "/vio-chat-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b5d42747b4b3ec8a97f",
+    "url": "/vio-chat-app/static/js/2.07ffe925.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vio-chat-app/static/js/2.07ffe925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9d4d11f8b3981e4432",
+    "url": "/vio-chat-app/static/js/main.4fbac980.chunk.js"
+  },
+  {
+    "revision": "58724e1aa99d4fa2e922",
+    "url": "/vio-chat-app/static/js/runtime-main.d12dc4e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vio-chat-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
